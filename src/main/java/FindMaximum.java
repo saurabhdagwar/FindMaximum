3994fde486f0160002cd586822d057beebc04d22
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-23 09:56:17 +0530

    [Saurabh]Added: maximumFloat method and three test cases to find maximum float number position

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,13 +1,25 @@
 public class FindMaximum {
-    public Integer maximumNumber(Integer firstnumber, Integer secondnumber, Integer thirdnumber){
+
+    public Integer maximumNumber(Integer firstnumber, Integer secondnumber, Integer thirdnumber) {
         Integer maxnumber = firstnumber;
-        if(secondnumber.compareTo(maxnumber)>0){
+        if (secondnumber.compareTo(maxnumber) > 0) {
             maxnumber = secondnumber;
         }
-        if(thirdnumber.compareTo(maxnumber)>0){
+        if (thirdnumber.compareTo(maxnumber) > 0) {
             maxnumber = thirdnumber;
         }
         return maxnumber;
+    }
+
+    public Double maximumFloat(Double firstfloat, Double secondfloat, Double thirdfloat) {
+        Double maxfloat = firstfloat;
+        if (secondfloat.compareTo(maxfloat) > 0) {
+            maxfloat = secondfloat;
+        }
+        if (thirdfloat.compareTo(maxfloat) > 0) {
+            maxfloat = thirdfloat;
+        }
+        return maxfloat;
     }
 
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -21,4 +21,23 @@ public class FindMaximumTest {
         int maximum = findMaximum.maximumNumber(20,33,40);
         Assert.assertEquals(40,maximum);
     }
+
+    @Test
+    public void givingFlots_shouldReturnMaxAt_firstPlace() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double maximum = findMaximum.maximumFloat(22.7,11.53,15.4);
+        Assert.assertEquals((Double) 22.7,maximum);
+    }
+    @Test
+    public void givingFlots_shouldReturnMaxAt_secondPlace() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double maximum = findMaximum.maximumFloat(15.4,22.7,11.53);
+        Assert.assertEquals((Double) 22.7,maximum);
+    }
+    @Test
+    public void givingFlots_shouldReturnMaxAt_thirdPlace() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double maximum = findMaximum.maximumFloat(11.53,15.4,22.7);
+        Assert.assertEquals((Double) 22.7,maximum);
+    }
 }
