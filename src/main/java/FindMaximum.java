284d102ea971918124765c8ef64166befa9bbc03
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-26 11:36:24 +0530

    [Saurabh]Refactored: Find maximum method can work on more than three element

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,33 +1,29 @@
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FindMaximum<E extends Comparable<E>> {
     E[] value;
-    List<E> listvaues = new LinkedList<>();
 
-      public FindMaximum(E[] value) {
-          this.value = value;
-      }
-
-      public FindMaximum(List<E> listvalues) {
-        this.listvaues = listvalues;
-    }
-    public E findmax(){
-        return Collections.max(listvaues);
+    public FindMaximum(E[] value) {
+        this.value = value;
     }
 
     public E findMaximumValue() {
-        if (value[0].compareTo(value[1]) > 0 && value[0].compareTo(value[2]) > 0) {
-            return value[0];
+        E[] maximumFind = value;
+        if (value.length <= 3) {
+            if (value[0].compareTo(value[1]) > 0 && value[0].compareTo(value[2]) > 0) {
+                return value[0];
+            } else if (value[1].compareTo(value[0]) > 0 && value[1].compareTo(value[2]) > 0) {
+                return value[1];
+            } else if (value[2].compareTo(value[0]) > 0 && value[2].compareTo(value[1]) > 0) {
+                return value[2];
+            }
         }
-         else if (value[1].compareTo(value[0]) > 0 && value[1].compareTo(value[2]) > 0) {
-            return value[1];
-        }
-         else {
-            return value[2];
-        }
+        else
+            Arrays.sort(maximumFind,Collections.reverseOrder());
+            return maximumFind[0];
+
     }
-
-
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -1,101 +1,88 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class FindMaximumTest {
     @Test
-    public void givingNumbers_shouldReturnMax_FirstPlace()  {
-        Integer[] values = {100 , 70 , 30 };
+    public void givingNumbers_shouldReturnMax_FirstPlace() {
+        Integer[] values = {100, 70, 30};
         Integer maximum = new FindMaximum<>(values).findMaximumValue();
-            Assert.assertEquals((Integer) 100 , maximum);
+        Assert.assertEquals((Integer) 100, maximum);
     }
+
     @Test
-    public void givingNumbers_shouldReturnMax_SecondPlace()  {
-        Integer[] values = {70 , 100 , 30 };
+    public void givingNumbers_shouldReturnMax_SecondPlace() {
+        Integer[] values = {70, 100, 30};
         Integer maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals((Integer) 100 , maximum);
+        Assert.assertEquals((Integer) 100, maximum);
     }
+
     @Test
-    public void givingNumbers_shouldReturnMax_ThirdPlace()  {
-        Integer[] values = {70 , 40 , 100 };
+    public void givingNumbers_shouldReturnMax_ThirdPlace() {
+        Integer[] values = {70, 40, 100};
         Integer maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals((Integer) 100 , maximum);
+        Assert.assertEquals((Integer) 100, maximum);
     }
+
     @Test
-    public void givingDouble_shouldReturnMax_FirstPlace()  {
-        Double[] values = {117.6 , 93.5 , 67.3 };
+    public void givingDouble_shouldReturnMax_FirstPlace() {
+        Double[] values = {117.6, 93.5, 67.3};
         Double maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals((Double) 117.6 , maximum);
+        Assert.assertEquals((Double) 117.6, maximum);
     }
+
     @Test
-    public void givingDouble_shouldReturnMax_SecondPlace()  {
-        Double[] values = {93.5 , 117.6 , 67.3 };
+    public void givingDouble_shouldReturnMax_SecondPlace() {
+        Double[] values = {93.5, 117.6, 67.3};
         Double maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals((Double) 117.6 , maximum);
+        Assert.assertEquals((Double) 117.6, maximum);
     }
+
     @Test
-    public void givingDouble_shouldReturnMax_ThirdPlace()  {
-        Double[] values = { 104.2, 93.5 , 117.6 };
+    public void givingDouble_shouldReturnMax_ThirdPlace() {
+        Double[] values = {104.2, 93.5, 117.6};
         Double maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals((Double) 117.6 , maximum);
+        Assert.assertEquals((Double) 117.6, maximum);
     }
+
     @Test
     public void givingString_shouldReturnMax_firstPlace() {
-        String values[] = {"Peach","Apple","Banana"};
+        String[] values = {"Peach", "Apple", "Banana"};
         String maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals("Peach", maximum  );
+        Assert.assertEquals("Peach", maximum);
     }
+
     @Test
     public void givingString_shouldReturnMax_secondPlace() {
-        String values[] = {"Apple","Peach","Banana"};
+        String[] values = {"Apple", "Peach", "Banana"};
         String maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals("Peach", maximum  );
+        Assert.assertEquals("Peach", maximum);
     }
+
     @Test
     public void givingString_shouldReturnMax_thirdPlace() {
-        String values[] = {"Banana","Apple","Peach"};
+        String[] values = {"Banana", "Apple", "Peach"};
         String maximum = new FindMaximum<>(values).findMaximumValue();
-        Assert.assertEquals("Peach", maximum  );
+        Assert.assertEquals("Peach", maximum);
     }
 
     @Test
     public void givingNumbers_shouldReturnMax() {
-        List<Integer> list = new LinkedList<>();
-        list.add(38);
-        list.add(63);
-        list.add(29);
-        list.add(79);
-        list.add(51);
-        list.add(69);
-        Object max = new FindMaximum<>(list).findmax();
-        Assert.assertEquals(79,max);
+        Integer[] values = {100, 70, 30, 70 ,130};
+        Integer maximum = new FindMaximum<>(values).findMaximumValue();
+        Assert.assertEquals((Integer) 130, maximum);
     }
+
     @Test
-    public void givingDouble_shouldReturnMax() {
-        List<Double> list = new LinkedList<>();
-        list.add(26.8);
-        list.add(43.7);
-        list.add(83.3);
-        list.add(52.8);
-        list.add(49.1);
-        list.add(69.5);
-        Object max = new FindMaximum<>(list).findmax();
-        Assert.assertEquals(83.3,max);
+    public void givingFloat_shouldReturnMax(){
+        Double[] values = {93.5, 117.6, 67.3, 133.9,88.4};
+        Double maximum = new FindMaximum<>(values).findMaximumValue();
+        Assert.assertEquals((Double) 133.9, maximum);
     }
+
     @Test
     public void givingString_shouldReturnMax() {
-        List<String> list = new LinkedList<>();
-        list.add("Banana");
-        list.add("Orange");
-        list.add("Kivi");
-        list.add("Strawberrie");
-        list.add("Peach");
-        Object max = new FindMaximum<>(list).findmax();
-        Assert.assertEquals("Strawberrie",max);
+        String[] values = {"Apple", "Peach", "Banana","Kivi","Strawberry"};
+        String maximum = new FindMaximum<>(values).findMaximumValue();
+        Assert.assertEquals("Strawberry", maximum);
     }
-
-
-
 }
